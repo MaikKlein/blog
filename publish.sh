8f8a808373a9a54a83a099e658e2efce3faaac71
Maik Klein <maikklein@googlemail.com>
2016-03-09 01:56:35 +0100

    update

--- publish.sh
+++ publish.sh
@@ -5,9 +5,8 @@ git push origin master
 hugo -d publish --theme=ghostwriter_custom --buildDrafts
 cd publish
 git init
-git remote add origin git@github.com:MaikKlein/blog.git
+git remote add origin git@github.com:MaikKlein/maikklein.github.io.git
 git add .
 git commit -m "update"
-git branch -m master gh-pages
-git push -f origin gh-pages
+git push -f origin master
 
